20ac2bda0d121d2304631f000096c2120b3af8c3
Nathan Dimmer <nathandimmer@gmail.com>
2020-06-14 19:14:32 -0400

    feat: moved state up

--- auto_accountability/lib/main.dart
+++ auto_accountability/lib/main.dart
@@ -3,14 +3,13 @@ import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
-var deviceStorage = {
+Map<String,List<Map<String,String>>> deviceStorage = {
   'current': [
     {
       'title': 'Goal 1',
       'description':
           'This is the description of Goal 1. This is the description of Goal 1.',
       'time': '6/11/2020 7:00 PM',
-      'done': null,
       'reflection': '',
       'key': '1'
     },
@@ -18,7 +17,6 @@ var deviceStorage = {
       'title': 'Goal 2',
       'description': 'This is the description of Goal 2',
       'time': '6/13/2020 9:00 AM',
-      'done': null,
       'reflection': '',
       'key': '2'
     },
@@ -26,7 +24,6 @@ var deviceStorage = {
       'title': 'Goal 3',
       'description': 'This is the description of Goal 3',
       'time': '6/15/2020 11:00 PM',
-      'done': null,
       'reflection': '',
       'key': '3'
     },
@@ -34,7 +31,6 @@ var deviceStorage = {
       'title': 'Goal 4',
       'description': 'This is the description of Goal 4',
       'time': '6/15/2020 11:00 PM',
-      'done': null,
       'reflection': '',
       'key': '4'
     },
@@ -42,12 +38,17 @@ var deviceStorage = {
       'title': 'Goal 5',
       'description': 'This is the description of Goal 5',
       'time': '6/15/2020 11:00 PM',
-      'done': null,
       'reflection': '',
       'key': '5'
     },
   ],
-  'past': []
+  'past': [{
+      'title': 'Goal 5',
+      'description': 'This is the description of Goal 5',
+      'time': '6/15/2020 11:00 PM',
+      'reflection': '',
+      'key': '5'
+    },]
 };
 
 class MyApp extends StatelessWidget {
@@ -71,16 +72,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, String>> currentList = deviceStorage['current'] as List<Map<String, String>>;
+  List<Map<String, String>> pastList = deviceStorage['past'] as List<Map<String, String>>;
+
+  void addGoal(Map content) {
+    currentList.add(content);
+  }
+
+  void currentToPast(Map itemObject) {
+    currentList.remove(itemObject);
+    pastList.add(itemObject);
+  }
+
+  void pastToCurrent(Map itemObject) {
+    currentList.add(itemObject);
+    pastList.remove(itemObject);
+  }
+
+  void delete(Map itemObject) {
+    pastList.remove(itemObject);
+  }
+
+  void reorderPast(int index1, int index2, Map orgIndex1) {
+    pastList.insert(index2, orgIndex1);
+    pastList.removeAt(index1);
+  }
+
+  void reorderCurrent(int index1, int index2, Map orgIndex1) {
+    currentList.insert(index2, orgIndex1);
+    currentList.removeAt(index1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Auto Accountability"),
       ),
-      body: Home(),
+      body: PageView(
+        children: <Widget>[
+          CurrentPage(
+            pastList: pastList,
+            currentList: currentList,
+            currentToPast: currentToPast,
+            reorderCurrent: reorderCurrent,
+          ),
+          PastPage(
+            pastList: pastList,
+            currentList: currentList,
+            delete: delete,
+            pastToCurrent: pastToCurrent,
+            reorderPast: reorderPast,
+          ),
+        ],
+        controller: pageController,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("Fab pressed");
+          Navigator.push(context,
+              MaterialPageRoute(builder: (BuildContext context) => NewGoal()));
         },
         tooltip: 'New goal',
         child: Icon(Icons.add),
@@ -140,152 +190,128 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
 final pageController = PageController(initialPage: 0);
 
-class _HomePageState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return (PageView(
-      children: <Widget>[
-        CurrentPage(),
-        PastPage(),
-      ],
-      controller: pageController,
-    ));
-  }
-}
+class CurrentPage extends StatelessWidget {
+  final List<Map<String, String>> pastList;
+  final List<Map<String, String>> currentList;
 
-class CurrentPage extends StatefulWidget {
-  @override
-  _CurrentPageState createState() => _CurrentPageState();
-}
+  final Function currentToPast;
+  final Function reorderCurrent;
 
-class _CurrentPageState extends State<CurrentPage> {
-  var currentList = deviceStorage['current'];
-  var pastList = deviceStorage['past'];
+  CurrentPage(
+      {this.pastList,
+      this.currentList,
+      this.currentToPast,
+      this.reorderCurrent});
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> items = currentList.map((itemsObject) {
+    var items = currentList.map((itemsObject) {
       return (Dismissible(
-        child: Padding(
-          child: Card(
-            child: ListTile(
-              title: Text(itemsObject['title']),
-              subtitle: ConstrainedBox(
-                constraints: BoxConstraints.loose(Size(double.infinity,
-                    32)), //TODO: Replace with a dynamic solution. Size.copy()?
-                child: Column(
+          child: Padding(
+            child: Card(
+              child: ListTile(
+                title: Text(itemsObject['title']),
+                subtitle: ConstrainedBox(
+                  constraints: BoxConstraints.loose(Size(double.infinity,
+                      32)), //TODO: Replace with a dynamic solution. Size.copy()?
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        itemsObject['description'],
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(itemsObject['time'],
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                          )),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                ),
+                isThreeLine: true,
+                trailing: Column(
                   children: <Widget>[
-                    Text(
-                      itemsObject['description'],
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(itemsObject['time'],
-                        style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                        )),
+                    Icon(Icons.drag_handle),
+                    IconButton(
+                      icon: Icon(Icons.share),
+                      visualDensity:
+                          VisualDensity(horizontal: -4, vertical: -4),
+                      padding: EdgeInsets.all(0),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            barrierDismissible: true,
+                            builder: (context) {
+                              return (AlertDialog(
+                                title: Text('Share this goal'),
+                                content: Column(
+                                  children: <Widget>[
+                                    Text(
+                                        'Share this goal with a friend so they can be an accountability buddy!'),
+                                    TextField(
+                                      autofocus: true,
+                                      keyboardType: TextInputType.phone,
+                                      decoration: InputDecoration(
+                                          labelText: 'Phone number'),
+                                    )
+                                  ],
+                                ),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    onPressed: () {
+                                      print('Phone number canceled');
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Cancel'),
+                                  ),
+                                  RaisedButton(
+                                    onPressed: () {
+                                      print('Phone number entered');
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Send'),
+                                    color: Colors.blue[500],
+                                    textColor: Colors.white,
+                                  ),
+                                ],
+                              ));
+                            });
+                      },
+                    )
                   ],
-                  crossAxisAlignment: CrossAxisAlignment.start,
                 ),
               ),
-              isThreeLine: true,
-              trailing: Column(
-                children: <Widget>[
-                  Icon(Icons.drag_handle),
-                  IconButton(
-                    icon: Icon(Icons.share),
-                    visualDensity: VisualDensity(horizontal: -4, vertical: -4),
-                    padding: EdgeInsets.all(0),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          barrierDismissible: true,
-                          builder: (context) {
-                            return (AlertDialog(
-                              title: Text('Share this goal'),
-                              content: Column(
-                                children: <Widget>[
-                                  Text(
-                                      'Share this goal with a friend so they can be an accountability buddy!'),
-                                  TextField(
-                                    autofocus: true,
-                                    keyboardType: TextInputType.phone,
-                                    decoration: InputDecoration(
-                                        labelText: 'Phone number'),
-                                  )
-                                ],
-                              ),
-                              actions: <Widget>[
-                                FlatButton(
-                                  onPressed: () {
-                                    print('Phone number canceled');
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('Cancel'),
-                                ),
-                                RaisedButton(
-                                  onPressed: () {
-                                    print('Phone number entered');
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('Send'),
-                                  color: Colors.blue[500],
-                                  textColor: Colors.white,
-                                ),
-                              ],
-                            ));
-                          });
-                    },
-                  )
-                ],
-              ),
+              elevation: 2,
             ),
-            elevation: 2,
+            padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
           ),
-          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-        ),
-        key: UniqueKey(),
-        background: Container(
-          child: Padding(
-            child: Icon(Icons.check),
-            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+          key: UniqueKey(),
+          background: Container(
+            child: Padding(
+              child: Icon(Icons.check),
+              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            ),
+            color: Colors.green[400],
+            alignment: Alignment.centerLeft,
           ),
-          color: Colors.green[400],
-          alignment: Alignment.centerLeft,
-        ),
-        secondaryBackground: Container(
-          child: Padding(
-            child: Icon(Icons.close),
-            padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+          secondaryBackground: Container(
+            child: Padding(
+              child: Icon(Icons.close),
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+            ),
+            color: Colors.red[400],
+            alignment: Alignment.centerRight,
           ),
-          color: Colors.red[400],
-          alignment: Alignment.centerRight,
-        ),
-        direction: DismissDirection.horizontal,
-        onDismissed: (direction) {
-          if (direction == DismissDirection.startToEnd) {
-            setState(() {
-              pastList.add(itemsObject);
-              print(pastList);
-              currentList.remove(itemsObject);
-              print(currentList);
-            });
-          } else {
-            setState(() {
-              pastList.add(itemsObject);
-              print(pastList);
-              currentList.remove(itemsObject);
-              print(currentList);
-            });
-          }
-        },
-      ));
+          direction: DismissDirection.horizontal,
+          onDismissed: (direction) {
+            if (direction == DismissDirection.startToEnd) {
+              currentToPast(itemsObject);
+            } else {
+              currentToPast(itemsObject);
+            }
+          }));
     }).toList();
 
     return (Center(
@@ -298,10 +324,7 @@ class _CurrentPageState extends State<CurrentPage> {
                 index1++;
               }
 
-              setState(() {
-                currentList.insert(index2, orgIndex1);
-                currentList.removeAt(index1);
-              });
+              reorderCurrent(index1, index2, orgIndex1);
             },
             header: Padding(
               child: Text(
@@ -313,18 +336,24 @@ class _CurrentPageState extends State<CurrentPage> {
   }
 }
 
-class PastPage extends StatefulWidget {
-  @override
-  _PastPageState createState() => _PastPageState();
-}
+class PastPage extends StatelessWidget {
+  final List<Map<String, String>> pastList;
+  final List<Map<String, String>> currentList;
 
-class _PastPageState extends State<PastPage> {
-  var currentList = deviceStorage['current'];
-  var pastList = deviceStorage['past'];
+  final Function pastToCurrent;
+  final Function delete;
+  final Function reorderPast;
+
+  PastPage(
+      {this.pastList,
+      this.currentList,
+      this.delete,
+      this.pastToCurrent,
+      this.reorderPast});
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> items = pastList.map((itemsObject) {
+    var items = pastList.map((itemsObject) {
       return (Dismissible(
         child: Padding(
           child: Card(
@@ -374,14 +403,9 @@ class _PastPageState extends State<PastPage> {
         direction: DismissDirection.horizontal,
         onDismissed: (direction) {
           if (direction == DismissDirection.startToEnd) {
-            setState(() {
-              currentList.add(itemsObject);
-              pastList.remove(itemsObject);
-            });
+            pastToCurrent(itemsObject);
           } else {
-            setState(() {
-              pastList.remove(itemsObject);
-            });
+            delete(itemsObject);
           }
         },
       ));
@@ -397,10 +421,7 @@ class _PastPageState extends State<PastPage> {
                 index1++;
               }
 
-              setState(() {
-                currentList.insert(index2, orgIndex1);
-                currentList.removeAt(index1);
-              });
+              reorderPast(index1, index2, orgIndex1);
             },
             header: Padding(
               child: Text(
@@ -409,5 +430,32 @@ class _PastPageState extends State<PastPage> {
               ),
               padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
             ))));
+  }
+}
+
+class NewGoal extends StatelessWidget {
+  final Function addGoal;
+
+  NewGoal({this.addGoal});
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            addGoal({
+              'title': 'Added Goal',
+              'description': 'This is the description of the added goal',
+              'time': '6/17/2020 11:00 PM',
+              'reflection': '',
+            });
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Add a new goal'),
+      ),
+    ));
   }
 }
